49d428652b2a3215306eb47b7b865b3ed33326e9
Santiago Giraudin <gira1849@gmail.com>
2023-08-09 19:05:34 -0300

    validacion

--- js/validacion.js
+++ js/validacion.js
@@ -1,9 +1,28 @@
-function showAlertSuccess() {
-    document.getElementById("alert-success").classList.add("show");
-}
+document.addEventListener("DOMContentLoaded", function(){
 
-function showAlertError() {
-    document.getElementById("alert-danger").classList.add("show");
-}
+    function showAlertSuccess() {
+        document.getElementById("alert-success").classList.add("show");
+    }
+    
+    function showAlertError() {
+        document.getElementById("alert-danger").classList.add("show");
+    }
+    
+    let form = document.getElementById("formulario");
+    
+    let pass1 = document.getElementById("password1").value;
+    let pass2 = document.getElementById("password2").value;
 
-const formulario = document.getElementById()
+    form.addEventListener('submit', function(event) {
+        event.preventDefault();
+    
+        if(pass1 == pass2)
+        {
+            showAlertSuccess();
+        }else{
+            showAlertError();
+        }
+    });
+
+})
+
